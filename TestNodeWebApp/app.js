220aca91c2d4ce5e108240fa9d82c997c6ea9e15
augus <augus@DESKTOP-KK3N7C7>
2017-01-02 16:20:48 +0100

    OK

---
+++ TestNodeWebApp/app.js
@@ -0,0 +1,40 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+var fs = require('fs');
+
+var express = require('express');
+var path = require("path");
+var bodyParser = require('body-parser');
+
+//CREATE APP
+var app = express();
+app.use(express.static('css'));
+app.use(express.static('scripts'));
+app.use(bodyParser.json());
+
+//LOCATION OF STATIC CONTENT IN YOUR FILESYSTEM
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.post('/', function (req, res) {
+
+    var calculation = req.body.data;
+    var response = new Object();
+    try {
+        response.result =  eval(calculation);
+        response.success = true;
+    }
+    catch (err) {
+        response.result = err.message;
+        response.success = false;
+    }
+    res.send(JSON.stringify(response));
+
+});
+
+console.log('Calculator web application running');
+
+//PORT TO LISTEN TO
+app.listen(1337);
---
+++ TestNodeWebApp/scripts/calculator.js
@@ -0,0 +1,44 @@
+﻿
+$(document).ready(function () {
+    $('.pure-button').click(function () {
+    //execute calculation
+        var clickedButton = $(this)[0];
+
+        var text = clickedButton.innerHTML;
+
+        $('#calculation').val($('#calculation').val() + text);
+        executeServerCalculation();
+    });
+
+    $('#calculation').change(function () {
+        
+        executeServerCalculation();
+    });
+
+
+});
+
+function executeServerCalculation() {
+    var calculation = $('#calculation').val();
+    calculation = calculation.replace('x', '*');
+    $.ajax({
+        url: 'http://localhost:1337/',
+        contentType: 'application/json',
+        data: '{"data": "' + calculation + '"}',
+        type: 'POST',
+        jsonpCallback: 'callback', // this is not relevant to the POST anymore
+        success: function (data) {
+            var ret = jQuery.parseJSON(data);
+
+            if (ret.success) {
+                $('#result').val(ret.result);
+                $('#message').html("Success");
+            }
+            else $('#message').html("Error: " + ret.result);
+            //console.log('Success: ')
+        },
+        error: function (xhr, status, error) {
+            alert('Error: ' + error.message);          
+        },
+    });
+}
