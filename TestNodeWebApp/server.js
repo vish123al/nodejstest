4380428733f6bd037a95f9fcbd25203f1c126a29
vish123al <vishaldenge1@gmail.com>
2017-09-25 17:00:53 +0530

    Update server.js

--- TestNodeWebApp/server.js
+++ TestNodeWebApp/server.js
@@ -1,17 +1,17 @@
-'use strict';
+
 
 
 // Constants
-const PORT = 8080;
-const ADMIN_PORT = 8081;
-const SUPER_USER_PORT = 8082;
+var PORT = 8080;
+var ADMIN_PORT = 8081;
+var SUPER_USER_PORT = 8082;
 
 var express = require('express');
 var path = require("path");
 var bodyParser = require('body-parser');
 
 //CREATE APP
-const app = express();
+var app = express();
 app.use(express.static('css'));
 app.use(express.static('scripts'));
 app.use(bodyParser.json());
@@ -19,27 +19,15 @@ app.use(bodyParser.json());
 
 //LOCATION OF STATIC CONTENT IN YOUR FILESYSTEM
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/home.html'));
-    //__dirname : It will resolve to your project folder.
+  res.send('Hello world! I am the regular app\n');
 });
 
 app.listen(PORT);
 console.log('Running on ' + PORT);
 
-// Admin App
-const adminApp = express();
-adminApp.get('/', function (req, res) {
-  res.send('Hello world! I am the admin app\n');
-});
 
-adminApp.listen(ADMIN_PORT);
-console.log('Running on ' + ADMIN_PORT);
 
-// Super User App
-const superUserApp = express();
-superUserApp.get('/', function (req, res) {
-  res.send('Hello world! I am the super user app\n');
-});
 
-superUserApp.listen(SUPER_USER_PORT);
-console.log('Running on ' + SUPER_USER_PORT);
+
+
+
