daccbbc91d931c06155d0651bffd7357cc86459e
vish123al <vishaldenge1@gmail.com>
2017-09-25 15:34:31 +0530

    Update server.js

--- TestNodeWebApp/server.js
+++ TestNodeWebApp/server.js
@@ -1,6 +1,35 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\nThis is node.js');
-}).listen(port);
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const ADMIN_PORT = 8081;
+const SUPER_USER_PORT = 8082;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world! I am the regular app\n');
+});
+
+app.listen(PORT);
+console.log('Running on ' + PORT);
+
+// Admin App
+const adminApp = express();
+adminApp.get('/', function (req, res) {
+  res.send('Hello world! I am the admin app\n');
+});
+
+adminApp.listen(ADMIN_PORT);
+console.log('Running on ' + ADMIN_PORT);
+
+// Super User App
+const superUserApp = express();
+superUserApp.get('/', function (req, res) {
+  res.send('Hello world! I am the super user app\n');
+});
+
+superUserApp.listen(SUPER_USER_PORT);
+console.log('Running on ' + SUPER_USER_PORT);
